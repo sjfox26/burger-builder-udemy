6c75166e6af826ff82c43287d25aaf57c297d023
Scott Fox <scottfox87@gmail.com>
2018-05-16 13:40:55 -0500

    output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            }); //returns an array with the length of whatever the value is of the given key in the ingredients object
+        });
+
+    // line 7- transform ingredients object from object into an array...returns ["salad", "cheese", "bacon", "meat"]
+    // line 8- igKey will be "salad", and so on
+    // line 9- returns an array with blank spots for the number(value) in the array... e.g, salad: 2 would be [ , ]...<< Array(ingredients["salad"] = Array(2)   really the length is the important thing here, since it's used by next map function
+    // line 10- [ , ] (with "salad" as igKey) gets mapped with an index, so the key is "salad 1" and type is "salad" on first time through map, then "salad 1" key with "salad" type for another BurgerIngredient component
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 1,
+            meat: 2
+        }
+    }
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
