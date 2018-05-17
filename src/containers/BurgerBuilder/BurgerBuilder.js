0c4e2918598d3304176e6fa6e0dbd5110f2c1423
Scott Fox <scottfox87@gmail.com>
2018-05-17 15:33:16 -0500

    add backdrop behind modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients //copied ingredients object from state ^^^ in an immutable way
@@ -84,7 +88,7 @@ class BurgerBuilder extends Component {
         //{"salad": true, "meat": false, ...}
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
