3f47798ac743bde1b47705c8539656dbcff37771
Scott Fox <scottfox87@gmail.com>
2018-06-07 20:33:01 -0500

    add logic to output orders dynamically from server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,34 +91,11 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('Continuing on!')
-        /*this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Steph Curry',
-                address: {
-                    street: 'TestStreet',
-                    zipCode: '03030',
-                    country: 'USA'
-
-                },
-                email: 'test@test.com'
-
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false});
-            });*/
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
            pathname: '/checkout',
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            cheese: 1,
-            bacon: 1
-        }
+        ingredients: null,
+        price: 0
     }
     /* Dummy data ^^^ ...will need routing to really pass in the ingredients, which we'll add later*/
-    componentDidMount() {
+    componentWillMount () {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+
+            if (param[0] === 'price') {
+                price = param[1];
+            }
+            else {
+                // ['salad', '1']
+                ingredients[param[0]] = +param[1];
+            }
+
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -35,6 +42,9 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}/>
+                <Route
+                    path={this.props.match.path + '/contact-data'}
+                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)} />
             </div>
         );
     }
