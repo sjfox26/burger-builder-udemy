7ac8a5456123e7b56bb8c04e8cbdb2623cf684b5
Scott Fox <scottfox87@gmail.com>
2018-06-16 09:07:39 -0500

    start backend auth with token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD5yFwbHlwLeZ9rZIwmBswOJqsTiWnwFKs', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err))
+            });
     };
 };
