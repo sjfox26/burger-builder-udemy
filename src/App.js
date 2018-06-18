f0a62f12a12b7c102465a27dfb98299940cf23a0
Scott Fox <scottfox87@gmail.com>
2018-06-17 20:36:03 -0500

    add persistent auth state to localStorage checked by token and expiration date

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,24 +8,35 @@ import Checkout from "./containers/Checkout/Checkout";
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
-
+//https://stackoverflow.com/questions/48699820/how-do-i-hide-api-key-in-create-react-app
 console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Layout>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/" exact component={BurgerBuilder} />
-          </Layout>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/orders" component={Orders}/>
+                    <Route path="/auth" component={Auth}/>
+                    <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                </Layout>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
