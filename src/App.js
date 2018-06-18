d48b6f9ba841db38eafa150703cca492e4f8f02e
Scott Fox <scottfox87@gmail.com>
2018-06-17 21:11:58 -0500

    set up guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -11,7 +11,7 @@ import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
 //https://stackoverflow.com/questions/48699820/how-do-i-hide-api-key-in-create-react-app
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+//console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 class App extends Component {
     componentDidMount() {
@@ -19,19 +19,41 @@ class App extends Component {
     }
 
     render() {
+        let routes = (
+                <Switch>
+                    <Route path="/auth" component={Auth}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                    <Redirect to="/" />
+                </Switch>
+        );
+
+        if (this.props.isAuthenticated ) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/orders" component={Orders}/>
+                    <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+
         return (
             <div>
                 <Layout>
-                    <Route path="/checkout" component={Checkout}/>
-                    <Route path="/orders" component={Orders}/>
-                    <Route path="/auth" component={Auth}/>
-                    <Route path="/logout" component={Logout}/>
-                    <Route path="/" exact component={BurgerBuilder}/>
+                    {routes}
                 </Layout>
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -39,4 +61,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
