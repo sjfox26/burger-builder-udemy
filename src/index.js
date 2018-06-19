5e3681e3f69748d7d3e9f898fb7353cfe76648cb
Scott Fox <scottfox87@gmail.com>
2018-06-19 07:59:41 -0500

    add basepath to browser router

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/burger-builder-udemy">
             <App />
         </BrowserRouter>
     </Provider>
